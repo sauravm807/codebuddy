6db56dc7a049dc7e60b163ebdc14c48f95c6abd5
saurav vishal <sauravvishal@globussoft.in>
2022-11-18 15:40:37 +0530

    .

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,42 +4,51 @@ module.exports.getUsersWithPostCount = async (req, res) => {
     try {
         //TODO: Implement this API
         let { page, limit } = req.query;
-        page = Number(page);
-        limit = Number(limit);
-        const offset = (page - 1) * limit;
+        const count = await User.countDocuments()
+        page = Number(page) || 0;
+        limit = Number(limit) || count;
+        const offset = page ? (page - 1) * limit : 0;
 
-        const data = await User.aggregate([
+        let data = await User.aggregate([
             {
                 $lookup: {
-                    from: "posts",
-                    foreignField: "userId",
-                    localField: "_id",
-                    as: "posts"
+                    from: 'posts',
+                    let: { user_id: "$_id" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $eq: ["$userId", "$$user_id"]
+                                }
+                            }
+                        }], as: "posts"
                 }
             },
             {
                 $project: {
-                    name: 1, last_name: 1,
-                    posts: { $size: "$posts" }
+                    _id: "$_id",
+                    name: 1,
+                    posts: {
+                        $size: "$posts"
+                    }
                 }
-            }
-        ]);
-        
+            }]).skip(offset).limit(limit);
+
         let resData;
         if (page && limit) {
             resData = {
                 data: {
-                    users: data.slice(offset, offset + limit),
+                    users: data,
                     pagination: {
-                        totalDocs: data.length,
+                        totalDocs: count,
                         limit: limit,
                         page: page,
-                        totalPages: Math.floor(data.length / limit),
+                        totalPages: Math.floor(count / limit),
                         pagingCounter: page,
                         hasPrevPage: page === 1 ? false : true,
-                        hasNextPage: page === Math.floor(data.length / limit) ? false : true,
+                        hasNextPage: page === Math.floor(count / limit) ? false : true,
                         prevPage: page === 1 ? null : page - 1,
-                        nextPage: page === Math.floor(data.length / limit) ? null : page + 1
+                        nextPage: page === Math.floor(count / limit) ? null : page + 1
                     }
                 }
             }
